930773ad15865ce902c6d3bd5c37af9d52d1b73f
Nurulloh99 <nurullohlutfullayev1999@gmail.com>
2025-04-10 10:59:28 +0500

    done

--- src/ConsoleApp1/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,7 +13,13 @@
 
         public static void DoSmth()
         {
-
+            var res1 = 45 + 45;
+            var res2 = 45 + 45;
+            var res3 = 45 + 45;
+            var res4 = 45 + 45;
+            var res5 = 45 + 45;
+            var res6 = 45 + 45;
+            var res7 = 45 + 45;
         }
     }
 }
